36f4ccaa23947760ac31106e14d0cb6e37a87669
llsilveira-sdm2 <llsilveira-sdm2@users.noreply.github.com>
2015-08-20 14:00:28 -0300

    Basic interface for the application

--- AndroidBeaconExample/app/src/main/java/com/github/llsilveira_sdm2/androidbeaconexample/ShowBeaconStatusActivity.java
+++ AndroidBeaconExample/app/src/main/java/com/github/llsilveira_sdm2/androidbeaconexample/ShowBeaconStatusActivity.java
@@ -28,8 +28,8 @@ public class ShowBeaconStatusActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_exit) {
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
